d96d3b56af0f997f8d6abce01fd2ad9986a68fb6
JonathanZ <zez451@usask.ca>
2025-11-07 21:56:03 -0600

    modified controller to build relations between MVC 3 classes.

--- Scripts/controller.gd
+++ Scripts/controller.gd
@@ -3,14 +3,18 @@ extends Node2D
 
 var model:Model
 var view:View
+var turn_label : Label # reference to a label node
 
+## Before the game starts, do a bunch of things to connect MVC together.
 func _ready() -> void:
+	turn_label = $UIturnLabel # denotes reference to a label node, which is in-game-label showing whose turn it currently is.
+	model = Model.new()
 	view = $TileMapLayer
-	view.connect("cell_selected", tile_selected)
-	model = Model.new(3, view)
-	view.set_model(model)
-	model.reset_board()
+	view.set_model(model) # relation between view and model
+	# Let the view notify controller when <signal omited> of "cell_selected" from view.gd
+	view.connect("cell_selected", Callable(self, "_on_cell_selected")) # relation between view and controller
 	
 	
-func tile_selected(coord:Vector2i) -> void:
-	model.toggle_tile(coord)
+func _on_cell_selected(coord: Vector2i):
+	model.make_move(coord) # relation between controller and model
+	
