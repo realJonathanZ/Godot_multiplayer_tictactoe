7dfadeed6296c82255c901ae29d7a3aba8686480
JonathanZ <zez451@usask.ca>
2026-08-28 14:17:14 -0600

    Verified that godot can receive right chat packet from server when there is one in ws stream.

--- Scripts/pywebdocTest.gd
+++ Scripts/pywebdocTest.gd
@@ -2,6 +2,8 @@ extends Node
 
 var socket: WebSocketPeer = WebSocketPeer.new()
 
+var has_sent_message: bool = false
+
 func _ready():
 	var error = socket.connect_to_url("ws://localhost:8765")
 	
@@ -16,18 +18,34 @@ func _process(_delta):
 	var state = socket.get_ready_state()
 		
 	if state == WebSocketPeer.STATE_OPEN:
-		print("checked in _process with current connection status to be: connected")
 		
-		var packet: Dictionary = {
-			"type": "chat",
-			"data": {
-				"sender": "Godot",
-				"message": "message from godot client"
-			}
-		} 
+		if not self.has_sent_message:
+			var packet: Dictionary = {
+				"type": "chat",
+				"data": {
+					"sender": "Godot",
+					"message": "message from godot client"
+				}
+			} 
+			
+			var json_message: String = JSON.stringify(packet)
+			
+			socket.send_text(json_message)
+			
+			print("Godot sent one chat packet")
+
+			has_sent_message = true # for not sending another pack		
 		
-		socket.send_text(JSON.stringify(packet))
-		
-		#set_process(false) # don't run my _process again and again after connected.
+			
+		# How many packets waiting in ws stream..
+		var available_packets_count: int = socket.get_available_packet_count()
+			
+		# if this client ever receives a message back..
+		if available_packets_count > 0:
+			var received_packet: PackedByteArray = socket.get_packet()
+			var received_message: String = received_packet.get_string_from_utf8()
+			
+			print("Godot received:", received_message)
+			
 			
 			
