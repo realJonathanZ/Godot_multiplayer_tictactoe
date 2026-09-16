d282d176d1720463a8acd21c65df390991c2f349
JonathanZ <zez451@usask.ca>
2026-09-16 02:12:31 -0600

    bruh I'm ran out and must stop. I record the problem that I wanna think of to be:
    Eventually I'm openning 2 godot clients on one PC, but with current playerid mechanism, these 2 ids are same, and it's so bad. I want to avoid this, at least on a test purpose!

--- network/network_adapter.gd
+++ network/network_adapter.gd
@@ -3,14 +3,26 @@
 ## define callback behavior(including communicate with server)
 
 ## Global NetworkAdaptor, receiving signals from somewhere else(E.X. message bus), then
-## do something based on the received signal.
+## do networking relevant tasks based on the received signal.
 ## Autoload name to be: NetAdp
-class_name NetworkAdaptor
+class_name NetworkAdapter
 extends Node
 
 func _ready() -> void:
 	MesB.local_move_requested.connect(_on_local_move_requested)
 	
 func _on_local_move_requested(coord: Vector2i) -> void:
-	print("[NetworkAdaptor] received local move request: ", coord)
+	var packet: Dictionary = {
+		"type": "xo_put",
+		"data": {
+			"position": {
+				"x": coord.x,
+				"y": coord.y,
+			},
+		},
+	}
+	
+	var json_message: String = JSON.stringify(packet)
+	
+	print("[NetworkAdapter] outgoing JSON(in str form):", json_message)
 	
